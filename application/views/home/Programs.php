ac639b338101b35d4aca8b74aedf33e99b240056
Shakeb Ameen <shakebameen@gmail.com>
2018-07-28 13:39:23 +0530

    Added activities and programs

--- application/views/home/Programs.php
+++ application/views/home/Programs.php
@@ -1,21 +1,22 @@
 <?php include "header.php"; ?>
-<?php  echo str_repeat('<br/>',3); ?>
+<?php  echo str_repeat('<br/>',4); ?>
 
 
     <section>
         <div class="container">
-            <h1>Our Programs</h1>
+            <h1 class="h1" style="color: #EB2D90">Programs</h1>
         </div>
     </section>
+
     <?php  echo str_repeat('<br/>',2); ?>
 
     <div class="row">
-        <div class="column">
+        <div class="column" >
             <div class="card hover-shadow">
-                <div class="container" style="margin-bottom: 10px">
+                <div class="container" style="margin-bottom: 10p; padding: 10px 10px 10px 10px">
                     <h3>Early Intervention Program</h3>
                     <p class="lead">
-  Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+  A One line description.
 </p>
                 <button type="button" class="btn btn-primary btn-sm">Enquire</button>
                 </div>
@@ -24,10 +25,10 @@
 
         <div class="column">
             <div class="card hover-shadow">
-                <div class="container" style="margin-bottom: 10px">
+                <div class="container" style="margin-bottom: 10px; padding: 10px 10px 10px 10px">
                     <h3>Vocational Training</h3><br>
                     <p class="lead">
-  Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+  A One line description.
 </p>
                 <button type="button" class="btn btn-primary btn-sm">Enquire</button>
                 </div>
@@ -36,10 +37,10 @@
 
         <div class="column">
             <div class="card hover-shadow">
-                <div class="container" style="margin-bottom: 10px">
+                <div class="container" style="margin-bottom: 10px; padding: 10px 10px 10px 10px">
                     <h3>Training Provider</h3><br>
                     <p class="lead">
-  Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+  A One line description.
 </p>
                 <button type="button" class="btn btn-primary btn-sm">Enquire</button>
                 </div>
@@ -48,5 +49,53 @@
         </div>
     </div>
     
+    <?php  echo str_repeat('<br/>',2); ?>
+    <section>
+        <div class="container">
+            <h2 class="h1" style="color: #EB2D90">Initiatives</h2>
+        </div>
+    </section>
+    <?php  echo str_repeat('<br/>',2); ?>
+    
+    <div class="container">
+        <div class="alert alert-secondary">
+           <dl class="row">
+                <dt class="col-sm-3">KWIZ-bel</dt>
+                <dd class="col-sm-9">An inter school quiz competetion.</dd>
+                <dt class="col-sm-3">FUN FIESTA</dt>
+                <dd class="col-sm-9">A summer vacation course.</dd>
+                <dt class="col-sm-3">HARMONY-bel</dt>
+                <dd class="col-sm-9">A cultural event.</dd>
+                <dt class="col-sm-3">JEEVIKA</dt>
+                <dd class="col-sm-9">A unique haat for woman entrepreneurship.</dd>
+                <dt class="col-sm-3">CHOCOLACA</dt>
+                <dd class="col-sm-9">A sustainable programme. (handmade chocolates and confectionaries)</dd>
+           </dl>
+        </div>
+      </div>
+
+      <?php  echo str_repeat('<br/>',2); ?>
+    <section>
+        <div class="container">
+            <h2 class="h1" style="color: #EB2D90">Activities</h2>
+        </div>
+    </section>
+    <?php  echo str_repeat('<br/>',2); ?>
+    
+    <div class="container">
+        <div class="alert alert-secondary">
+           <ul>
+                <li>Fun based education</li>
+                <li>Physiotherapy</li>
+                <li>Neuro-Developmental Therapy</li>
+                <li>Speech Therapy</li>
+                <li>Sensory Integration Therapy</li>
+                <li>Occupational Therapy</li>
+                <li>Alternate Therapies - Dance | Music | Arts</li>
+                <li>Sports - Table Tennis | Karrate | Chess (By S.R. Chess Academy)</li>
+           </ul>
+        </div>
+      </div>
+      
 
 <?php include "footer.php"; ?>
