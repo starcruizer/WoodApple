790c78a92d736298778315535d6b37d9b38b52d2
Shakeb Ameen <shakebameen@gmail.com>
2018-07-28 16:01:07 +0530

    Added Google Maps

--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -7,13 +7,12 @@
 
 <div class="row">
 	<div class="col-sm-4">
-		Careers <br/>
-		Volunteer <br/>
-		Events <br/>
+		<b>Locate us :</b>
+		<div id="map" style="height: 150px; width: 100%;"></div>
 	</div>
 	<div class="col-sm-4">
 		<b>Contact:</b> <br/><br/>
-		+91 - 1234567890 <br/>
+		+91 92383 00403 <br/>
 		support@wood-apple.co.in
 	</div>
 	<div class="col-sm-4">
@@ -41,15 +40,26 @@
 </section>
 
 
-
-
-
-
+<!-- JS Script for GMaps -->
+<script>
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 20.298463, lng: 85.806770};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+</script>
+<!-- JS Script for GMaps -->
 
 <!-- JS Scripts-->
 <script type="text/javascript" src="<?= base_url('assets/js/jquery.js'); ?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/js/bootstrap.js'); ?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/js/home.js'); ?>"></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7RpNL2_uC6XxYjutlLHvRz4oF-ADT6Qs&callback=initMap"></script>
 <!-- JS Scripts-->
 
 
