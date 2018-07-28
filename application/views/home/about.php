676c38ad4d7bae198c1c2b0e78381fa641be66dc
Shakeb Ameen <shakebameen@gmail.com>
2018-07-28 15:33:09 +0530

    Added about page

--- application/views/home/about.php
+++ application/views/home/about.php
@@ -1,7 +1,49 @@
 <?php include 'header.php'; ?>
 <br><br><br>
-<div class="container">
-	<span class="glyphicon glyphicon-factory"></span>
-</div>
+
+	<section>
+        <div class="container">
+            <h1 style="color: #EB2D90">About Us</h1>
+        </div>
+	</section>
+	
+	<?php  echo str_repeat('<br/>',1); ?>
+
+	<div class="container">
+		<p class="p">
+				SSNT(Shree Sai Niketan Trust) has now ventured out for a school for special children to reach out to the special needs of visually impaired, hearing impaired, mentally challenged, autism, cerebral palsy and multiple disabilities children
+		</p>
+		<p>Wood apple – A preparatory school for special children aims at all round development of the kids and making them competent in the worldly affair. Along with basic education it gives special attention to groom the children in their interest area like art, craft, music, single language learning, photography, choreography and many more. Work sheets are prepared taking care of each child’s need and area of development. It is based on 100% one to one teaching learning method.</p>
+		
+	</div>
+
+	<div class="container">
+			<div class="alert alert-secondary">
+				<p>Wood Apple cater special education, that is educating students with special educational needs, be it </p>
+				<ul>
+					<li>Visually impaired</li>
+					<li>Hearing impaired</li>
+					<li>Mentally challenged</li>
+					<li>Autusim</li>
+					<li>Cerebral Palsy</li>
+				</ul>
+				<p>We provide individualized education, addressing specific needs. We provide special features like -</p>
+				<ul>
+					<li>Low student to educator ratio.</li>
+					<li>Soft play areas</li>
+					<li>Sensory rooms</li>
+					<li>Developmental, corrective and supportive services</li>
+					<li>Services like - 
+						<ul>
+							<li>Physiological, counselling services</li>
+							<li>Physical and Occupational Therapies</li>
+							<li>Medical Services</li>
+							<li>Parent Counselling and training</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		  </div>
+
 <br>
 <?php include 'footer.php'; ?>
